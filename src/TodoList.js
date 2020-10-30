b9627de787574b67c845fb11017be00957d9d6cf
“Simran <itsimmi5@gmail.com>
2020-10-30 12:57:50 -0400

    Feat: Saving input in state

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,11 +1,25 @@
-import React, { Component } from 'react'
+import React, {useState} from 'react';
 
-export default class TodoList extends Component {
-    render() {
-        return (
-            <div>
-                hii this is TodoList
-            </div>
-        )
+export default function TodoList() {
+
+    const [list, addItem] = useState([]);
+    const [item, addInput] = useState();
+
+    let onSubmit = event => {
+        event.preventDefault();
+        addItem(list.concat(item));
     }
+
+    console.log(list);
+    return (
+        <div>
+            <form>
+                <label>
+                    Add item
+                    <input type="text" name='item' onChange={(event) => addInput(event.target.value)}/>
+                </label>
+                <button type="submit" onClick={onSubmit}> Submit </button>
+            </form>
+        </div>
+    )
 }
