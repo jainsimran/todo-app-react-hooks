8f7d48efa35a0161a73d105d7e344e3bfbd36a1a
“Simran <itsimmi5@gmail.com>
2020-11-02 15:18:15 -0500

    Feat: adding input item in list and display it on screen.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import './App.css';
-import TodoList from './TodoList';
+import React, { useState } from 'react';
+// import TodoList from './TodoList';
 
-function App() {
+export default function App() {
+
+  const [list, addItem] = useState([]);
+  const [item, setInput] = useState();
+
+  let onSubmit = event => {
+    event.preventDefault();
+    list.push(item);
+    console.log(list);
+    setInput('');
+  }
+
   return (
+ 
     <div>
-      <TodoList/>
-    </div>
-  );
-}
+      <form>
+        <label>
+          Add item
+          <input type="text" name='item' onChange={(event) => setInput(event.target.value)} value={item} />
+        </label>
+        <button type="submit" onClick={onSubmit}> Submit </button>
+      </form>
+      {list.length > 0 ? <h1>TODO LIST</h1> : null}
+      {list.map((items) => <li>{items}</li>)}
 
-export default App;
+
+      {/* <TodoList todoList={list} /> */}
+    </div>
+  )
+}
